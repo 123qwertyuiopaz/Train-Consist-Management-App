79159d2dacbc4dc14620fdda7f7aaf4500c9299c
123qwertyuiopaz <thejeshreddy2007@outlook.com>
2026-04-01 14:40:16 +0530

    UC4

--- src/trainconsistmangementapp.java
+++ src/trainconsistmangementapp.java
@@ -1,30 +1,27 @@
-import java.util.HashSet;
-import java.util.Set;
-//usecase 3
+
 public class TrainConsistmangementApp {
 
         public static void main(String[] args) {
-
+//usecase4
                 System.out.println("====================================");
-                System.out.println("UC3 - Track Unique Bogie IDs");
+                System.out.println("UC4 - Maintain Ordered Bogie Consist");
                 System.out.println("====================================\n");
+                List<String> trainConsist = new LinkedList<>();
+                trainConsist.add("Engine");
+                trainConsist.add("Sleeper");
+                trainConsist.add("AC");
+                trainConsist.add("Cargo");
+                trainConsist.add("Guard");
+                System.out.println("Initial Train Consist:");
+                System.out.println(trainConsist);
+                trainConsist.add(2, "Pantry Car");
+                System.out.println("\nAfter Inserting 'Pantry Car' at position 2:");
+                System.out.println(trainConsist);
+                ((LinkedList<String>) trainConsist).removeFirst();
+                ((LinkedList<String>) trainConsist).removeLast();
+                System.out.println("\nAfter Removing First and Last Bogie:");
+                System.out.println(trainConsist);
 
-                Set<String> bogies = new HashSet<>();
-
-                bogies.add("BG101");
-                bogies.add("BG102");
-                bogies.add("BG103");
-                bogies.add("BG104");
-
-                bogies.add("BG101");
-                bogies.add("BG102");
-
-                System.out.println("Bogie IDs After Insertion:");
-                System.out.println(bogies);
-
-                System.out.println("\nNote:");
-                System.out.println("Duplicates are automatically ignored by HashSet.");
-
-                System.out.println("\nUC3 uniqueness validation completed...");
+                System.out.println("\nUC4 ordered consist operations completed...");
         }
 }
